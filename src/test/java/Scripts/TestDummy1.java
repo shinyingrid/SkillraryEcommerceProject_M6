aecbab5e48fbecae132f67eabef6eae67fccaf51
Shiny Ingrid C <Shiny Ingrid C@DESKTOP-3RHQS1L>
2023-09-07 12:07:02 +0530

    DummyScript

---
+++ src/test/java/Scripts/TestDummy1.java
@@ -0,0 +1,16 @@
+package Scripts;
+
+import org.testng.annotations.Test;
+
+public class TestDummy1 
+{
+	
+		@Test
+		public void dummy()
+		{
+			System.out.println("Shiny - Welcome to Github");
+		}
+	
+}
+
+//We are creating a child branch to add to the master branch
